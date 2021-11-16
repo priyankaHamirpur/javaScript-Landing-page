be3533f8f3346be80e51c64f8c22efcdf846d85c
Priyanka <psharma0796@gmail.com>
2021-11-16 17:40:19 +0530

    first commit

---
+++ myscript.js
@@ -0,0 +1,19 @@
+
+  function openNav(){
+    document.getElementById('mySidenav').style.width="250px";
+
+  }
+
+  function closeNav(){
+      document.getElementById('mySidenav').style.width="0";
+  }
+  function openSignup(){
+  document.getElementById('my_form').style.display ="block";
+  }
+ 
+  function close_btn(){
+    document.getElementsById('my_form').style.display='none';
+
+  }
+    
+
